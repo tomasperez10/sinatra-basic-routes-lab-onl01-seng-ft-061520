001e318457b94d9b73ed543c3a64331bfcf0c92f
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 06:48:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Tomas"
+  end
+
+  get '/hometown' do
+    "My hometown is Atlanta"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Hop by A Tribe Called Quest"
+  end
 end
