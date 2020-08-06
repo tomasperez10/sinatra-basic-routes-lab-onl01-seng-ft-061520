a1e8da21f34813a4783f39603a87a6be8a80a1f8
Tomas Perez <tomasitopg6@gmail.com>
2020-08-06 06:46:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Alexis"
+  end
+
+  get '/hometown' do
+    "My hometown is Lakewood"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Castle of Glass"
+  end
 end
